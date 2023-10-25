5f5655001cf7a6bee8551299b238d47bea1f7f6b
Sanchari Ray <raysanchari930@gmail.com>
2023-10-25 22:36:57 +0530

    Update App.py

--- App.py
+++ App.py
@@ -14,7 +14,7 @@ import io
 import nltk
 nltk.download('punkt')
 
-st.set_page_config(page_title='InNews🇮🇳: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
+st.set_page_config(page_title='SummarizeToday: A Summarised News📰 Portal', page_icon='./Meta/newspaper.ico')
 
 
 def fetch_news_search_topic(topic):
@@ -83,7 +83,7 @@ def display_news(list_of_news, news_quantity):
 
 
 def run():
-    st.title("InNews🇮🇳: A Summarised News📰")
+    st.title("SummarizeToday: A Summarised News📰")
     image = Image.open('./Meta/newspaper.png')
 
     col1, col2, col3 = st.columns([3, 5, 3])
